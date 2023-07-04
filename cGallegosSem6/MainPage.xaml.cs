24ac3bf11505535976d984dd19b46f21e4ec38e8
HOME <HOME@DESKTOP-L6UBTV3>
2023-07-03 20:57:48 -0500

    Agregar archivos de proyecto.

---
+++ cGallegosSem6/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace cGallegosSem6
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.56.1/ws_uisrael/post.php";
+        private HttpClient client = new HttpClient();
+        private ObservableCollection<Estudiante> post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+        }
+        public async void ObtenerDatos()
+        {
+            var contenido = await client.GetStringAsync(Url);
+            List<Estudiante> listaPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+            post = new ObservableCollection<Estudiante>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Insertar());
+        }
+
+        private void listaEstudiantes_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var objeto = (Estudiante)e.SelectedItem;
+            var codigoTem = objeto.codigo.ToString();
+            int codigo = Convert.ToInt32(codigoTem);
+            string nombre = objeto.nombre.ToString();
+            string apellido = objeto.apellido.ToString();
+            var edadTemp = objeto.edad.ToString();
+            int edad = Convert.ToInt32(edadTemp);
+
+            Navigation.PushAsync(new ActualizarEliminar(codigo, nombre, apellido, edad));
+        }
+    }
+}
